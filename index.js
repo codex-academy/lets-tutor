c68df292de23d70655a9e9d282d528e6030e4caf
avermeulen <andre.vermeulen@gmail.com>
2017-05-01 17:44:52 +0200

    Add Subjects via the URL

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require('express');
+const SubjectRoutes = require('./subjects');
+
+const subjectRoutes = SubjectRoutes();
 
 const app = express();
 
 app.get('/', function(req, res){
     res.send('LetsTutor');
 });
+
+app.get('/subjects', subjectRoutes.index);
+app.get('/subjects/add/:subject', subjectRoutes.add);
 
 const port = 3007;
 
---
+++ subjects.js
@@ -0,0 +1,30 @@
+module.exports = function(){
+
+    const subjectList = [];
+
+    const index = function(req, res){
+        res.send(subjectList);
+    };
+
+    const add = function(req, res){
+        //res.send('Add a subject');
+
+        var subject = req.params.subject;
+        
+        var foundSubject = subjectList.find(function(currentSubject){
+            return currentSubject === subject;
+        });
+
+        if (subject && !foundSubject){
+            subjectList.push(subject);
+        }
+
+        res.redirect('/subjects');
+    }
+
+    return {
+        index,
+        add
+    }
+
+}
